382b4ae51556ffe665a1ef56b7661bc971e11806
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-02 03:33:02 +0600

    string-uppercase-lowercase

---
+++ string-uppercase-lowercase.js
@@ -0,0 +1,28 @@
+const school = 'RAJUK UTTARA MODEL SCHOOL';
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+const subjet = 'Chemistry';
+const book = 'chemistry';
+
+if (subjet === book) {
+    console.log ('Mile gese')
+}
+
+else {
+    console.log('Mile Nai.......:(');
+    
+}
+
+
+const drink = 'water';
+const liquid = '    water';
+if (drink.trim() === liquid.trim()) {
+    console.log('Cholo pani khai');
+    
+}
+
+else {
+    console.log('Pani khabo nah -.-');
+    
+}
---
+++ string.js
@@ -0,0 +1,30 @@
+// String is immutable = not changable 
+
+const country = 'Bangladesh'; // Used in regular basis
+const division = 'Dhaka'; // Used in regular basis
+const distric = 'Dhaka'; // Powerful and will use it
+const thana = new String('Mirpur'); // for Special Purpose only
+const capital ='Dhaka';
+
+// console.log (typeof country);
+// console.log (typeof division);
+// console.log (typeof distric);
+// console.log (typeof thana)
+
+
+// Array is Mutable = you can change the array elements
+
+//  const numbers = [10,20,30,40,50,60,70];
+//  numbers[2] = 50; 
+//  console.log (numbers);
+//  console.log (numbers.length);
+//  console.log (numbers[0]);
+//  console.log (capital.length);
+//  console.log (capital[3]);
+
+console.log(capital);
+capital[0] = 'F';
+console.log(capital);
+
+
+
