f60cb2c89da7bee5d2a468d427d73b9b44a771f9
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-02 12:31:28 +0600

    object

---
+++ object.js
@@ -0,0 +1,19 @@
+// Object is Non-Primitive
+
+const bottle = {
+    brand: 'Apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+const subject = {
+    name: 'JavaScript',
+    mentor: 'Jhankar Mahbub',
+    examDate: '03 August, 2025',
+    chapters: ['First', 'Second', 'Third'],
+    exam: {
+        name: 'Final Exam',
+        marks: 100,
+    }   
+}
